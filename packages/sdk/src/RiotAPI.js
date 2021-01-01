d801d015f7f8708ddad8b316e9eb4d6eacdc968d
Mark Miyashita <negativetwelve@gmail.com>
2021-01-01 03:07:27 -0800

    Create the Summoner class for fetching

--- packages/sdk/src/RiotAPI.js
+++ packages/sdk/src/RiotAPI.js
@@ -1,37 +1,38 @@
 // Libraries
-import API from 'handy-api';
+import API from "handy-api";
 
+// App
+import Summoner from "./modules/Summoner";
 
 /**
  * Represents the client that connects to the Riot Games API.
  */
 class RiotAPI {
-
   // --------------------------------------------------
   // Initialize
   // --------------------------------------------------
-  constructor({apiKey, region}) {
+  constructor({ apiKey, region }) {
     const baseUrl = `https://${region}.api.riotgames.com`;
     this.apiKey = apiKey;
-    this.api = new API({baseUrl});
+    this.api = new API({ baseUrl });
   }
 
   get headers() {
     return {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     };
   }
 
   // --------------------------------------------------
   // Request
   // --------------------------------------------------
-  async request({method, path, params}) {
+  async request({ method, path, params }) {
     return this.api.request({
       method,
       path,
       params: {
         ...params,
-        'api_key': this.apiKey,
+        api_key: this.apiKey,
       },
       headers: this.headers,
     });
@@ -40,8 +41,9 @@ class RiotAPI {
   // --------------------------------------------------
   // Methods
   // --------------------------------------------------
-
+  get summoner() {
+    return Summoner;
+  }
 }
-
 
 export default RiotAPI;
---
+++ packages/sdk/src/modules/Summoner.js
@@ -0,0 +1,49 @@
+/**
+ * Represents the Summoner
+ */
+class Summoner {
+  // --------------------------------------------------
+  // GET
+  // --------------------------------------------------
+  static async getBySummonerName({ summonerName }) {
+    const response = await client.request({
+      method: "GET",
+      path: `/lol/summoner/v4/summoners/by-name/${summonerName}`,
+      params: {},
+    });
+
+    const json = await response.json();
+    const { status } = response;
+
+    if (status >= 200 && status < 300) {
+      return new this(json);
+    }
+
+    // If the request failed, we need to throw an error with the text.
+    const error = new Error(response.statusText);
+
+    error.response = response;
+    throw error;
+  }
+
+  // --------------------------------------------------
+  // Properties
+  // --------------------------------------------------
+  constructor(props) {
+    this.props = props;
+  }
+
+  get id() {
+    return this.props.id;
+  }
+
+  get accountId() {
+    return this.props.accountId;
+  }
+
+  // --------------------------------------------------
+  // Methods
+  // --------------------------------------------------
+}
+
+export default Summoner;
